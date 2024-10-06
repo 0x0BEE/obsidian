d8af9bc9a811d7f14fd8ce34649e027938a044fa
Jesse Gerard Brands <jesse@jgbrands.nl>
2024-10-06 14:15:39 +0200

    build: added liburing dependency

---
+++ cmake/Finduring.cmake
@@ -0,0 +1,18 @@
+include(FindPackageHandleStandardArgs)
+
+find_library(uring_LIBRARY NAMES uring)
+find_path(uring_INCLUDE_DIR NAMES liburing.h)
+
+find_package_handle_standard_args(uring
+        REQUIRED_VARS uring_LIBRARY uring_INCLUDE_DIR)
+
+if (URING_FOUND)
+    mark_as_advanced(uring_LIBRARY)
+    mark_as_advanced(uring_INCLUDE_DIR)
+endif ()
+
+if (URING_FOUND AND NOT TARGET uring::uring)
+    add_library(uring::uring SHARED IMPORTED)
+    set_property(TARGET uring::uring PROPERTY IMPORTED_LOCATION ${uring_LIBRARY})
+    target_include_directories(uring::uring INTERFACE ${uring_INCLUDE_DIR})
+endif ()
