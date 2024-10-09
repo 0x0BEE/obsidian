f11d406ddbffbcde5df6905ffeaff1ecebad576a
Jesse Gerard Brands <jesse@jgbrands.nl>
2024-10-09 10:04:33 +0200

    build: small bug fix to liburing find_package script

--- cmake/Finduring.cmake
+++ cmake/Finduring.cmake
@@ -6,12 +6,12 @@ find_path(uring_INCLUDE_DIR NAMES liburing.h)
 find_package_handle_standard_args(uring
         REQUIRED_VARS uring_LIBRARY uring_INCLUDE_DIR)
 
-if (URING_FOUND)
+if (uring_FOUND)
     mark_as_advanced(uring_LIBRARY)
     mark_as_advanced(uring_INCLUDE_DIR)
 endif ()
 
-if (URING_FOUND AND NOT TARGET uring::uring)
+if (uring_FOUND AND NOT TARGET uring::uring)
     add_library(uring::uring SHARED IMPORTED)
     set_property(TARGET uring::uring PROPERTY IMPORTED_LOCATION ${uring_LIBRARY})
     target_include_directories(uring::uring INTERFACE ${uring_INCLUDE_DIR})
