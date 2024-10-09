1a5eb1295040e57f831478f7799c6749579aeffa
Jesse Gerard Brands <jesse@jgbrands.nl>
2024-10-09 10:04:53 +0200

    server: start rewrite of server core

--- server/src/main.c
+++ server/src/main.c
@@ -16,28 +16,9 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "obsidian/log.h"
-#include "obsidian/server.h"
-
 #include <assert.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main() {
-    struct obs_server* server = obs_server_create(&(struct obs_server_params){
-        .queue_depth = 32,
-        .max_connections = 1024,
-        .frame_pool_size = 2048 * 32,
-    });
-    obs_server_listen(server, 25565);
-    OBS_LOG_INFO("server", "Listening on port %d", 25565);
-    while (1) {
-        obs_server_poll(server);
-        clock_nanosleep(CLOCK_MONOTONIC, 0, &(struct timespec){
-                            .tv_nsec = 100000,
-                        }, NULL);
-    }
-    obs_server_close(server);
-    obs_server_destroy(server);
     return EXIT_SUCCESS;
 }
